a1e2456088d94191ff71e966f3f4521a334242b1
Arimusya <neiron474@gmail.com>
2024-11-28 16:19:37 +0600

    Update urls.py

--- static_portfolio/urls.py
+++ static_portfolio/urls.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from portfolio import views
-from todo import views
+from todo import views as todo_views  # Используем alias для todo.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', todo_views.home, name='home'),  # Изменено на todo_views
     path('blog/', include('blog.urls')),
-    path('signup/', views.signupuser, name='signupuser'),
-    path('login/', views.loginuser, name='loginuser'),
-    path('logout/', views.logoutuser, name='logoutuser'),
-    path('create/', views.createtodo, name='createtodo'),
-    path('current/', views.currenttodos, name='currenttodos'),
-    path('completed/', views.completedtodos, name='completedtodos'),
-    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
-    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
-    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+    path('signup/', todo_views.signupuser, name='signupuser'),
+    path('login/', todo_views.loginuser, name='loginuser'),
+    path('logout/', todo_views.logoutuser, name='logoutuser'),
+    path('create/', todo_views.createtodo, name='createtodo'),
+    path('current/', todo_views.currenttodos, name='currenttodos'),
+    path('completed/', todo_views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', todo_views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', todo_views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', todo_views.deletetodo, name='deletetodo'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
