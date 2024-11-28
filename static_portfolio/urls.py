8e44dd52ea01fe62c7fb197c354e9ac431a1ba5a
Arimusya <neiron474@gmail.com>
2024-11-28 16:21:10 +0600

    Update urls.py

--- static_portfolio/urls.py
+++ static_portfolio/urls.py
@@ -6,7 +6,8 @@ from todo import views as todo_views  # Используем alias для todo.v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', todo_views.home, name='home'),  # Изменено на todo_views
+    path('', todo_views.home, name='home'),
+    path('', todo_views.index, name='index'),# Изменено на todo_views
     path('blog/', include('blog.urls')),
     path('signup/', todo_views.signupuser, name='signupuser'),
     path('login/', todo_views.loginuser, name='loginuser'),
